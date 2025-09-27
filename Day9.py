9cf0d41efa654acbd666c8d038b365dc98b5da46
Diana Ramírez Gamboa <dianaramirezgamboa@outlook.com>
2025-09-26 22:26:58 -0600

    Create Day9.py
    This day was to create a biding auction and it was quite simple because I already knew how to use dictionaries and also because I was not afraid of googling functions to make what I envision.
    I still have to polish this code though for the float number for the biding but I will tackle that problem later.

---
+++ Day9.py
@@ -0,0 +1,32 @@
+import art
+
+# Initiating variables
+biding_auction = {}
+other_biters = True
+
+#While loop to create as many entries in the dictionary as posible
+while other_biters:
+    print(art.logo)
+    print("\nWelcome to the Blind Biding Auction.")
+    name = input("Please enter your name: ")
+    try:#Have to see how to make this part a while loop also to only get numbers and then keep going, maybe with an if function?
+        amount = float(input("Please enter your bid amount (only numbers allowed): "))
+    except ValueError:
+        print("Please enter a correct bid as a number")
+
+    #Appending the data to the dictionary
+    biding_auction[name] = amount
+
+    # Handling other biders
+    question = input("Is there any other biders? Please answer 'yes' or 'no' only: ").lower()
+    if question == "no":
+        print("\n"*100)
+        other_biters = False
+    else:
+        print("\n"*100)
+        continue
+
+#Cheking for the highest bid
+max_key = max(biding_auction, key=biding_auction.get)#the get function does not need the () or it will get an error
+max_value = biding_auction[max_key]
+print(f"The winner is {max_key} with a bid of {max_value}")
